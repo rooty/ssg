c15eba7173ea40392b61f3e390f1d99dc9d7b1ac
Black Cat <rooty@chm.od.ua>
2012-12-14 16:09:30 +0200

    add new app: stock

--- ssgweb/ssgweb/settings/__init__.py
+++ ssgweb/ssgweb/settings/__init__.py
@@ -11,8 +11,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
-#MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+PROJECT_PATH = os.path.join( os.path.realpath(os.path.dirname(__file__)),'../')
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -49,18 +48,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -115,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'raven.contrib.django',
+    'ssgweb.utils',
 )
 
 RAVEN_CONFIG = {
